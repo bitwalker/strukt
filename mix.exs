6d1b70d9eec53f1e06453ceb33866ace69c76be8
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2021-06-09 19:12:08 -0400

    version 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Strukt.MixProject do
   def project do
     [
       app: :strukt,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.11",
       description: description(),
       package: package(),
